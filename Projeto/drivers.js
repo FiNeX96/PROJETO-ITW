ef0260d25426a6ee7dc97a5234e1b5cf4448a83b
FiNeX96 <rodrigoaguiar96@ua.pt>
2022-01-18 20:57:54 +0000

    abc

--- Projeto/drivers.js
+++ Projeto/drivers.js
@@ -23,6 +23,13 @@ $(document).ready(function () {
     });
 });
     // Search feature
+    $("#SearchText").keyup(function(event) {
+        if (event.keyCode === 13) {
+            console.log("oi")
+            $("#button_search").click();
+        }
+    });
+    
     $('#button_search').click(function () {
         console.log ($('#SearchText').val());
         var nome = $('#SearchText').val();
