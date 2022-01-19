25b412fb78ccf1eaadbead7c6c771cc677fdc980
filipe.obrist@ua.pt <filipe.obrist@ua.pt>
2022-01-19 23:35:21 +0000

    as

---
+++ Projeto/tickets.js
@@ -0,0 +1,16 @@
+﻿var count = 0;
+
+$("#update1").click(function () {
+    count++;
+    $('#counter').text(count);
+})
+
+$("#update2").click(function () {
+    if (count <= 0) {
+        $('#counter').text(count);
+    }
+    else {
+        count--;
+        $('#counter').text(count);
+    }
+})
