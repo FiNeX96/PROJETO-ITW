b042da928c013b71955f7aeb8233eee296c2f4b7
filipe.obrist@ua.pt <filipe.obrist@ua.pt>
2022-01-19 23:45:22 +0000

    as

--- Projeto/tickets.js
+++ Projeto/tickets.js
@@ -1,16 +1,57 @@
-﻿var count = 0;
+﻿$('document').ready(function () {
 
-$("#update1").click(function () {
-    count++;
-    $('#counter').text(count);
+    var count = 0;
+
+    $("#update1").click(function () {
+        count++;
+        $('#counter').text(count);
+    })
+
+    $("#update2").click(function () {
+        if (count <= 0) {
+            $('#counter').text(count);
+        }
+        else {
+            count--;
+            $('#counter').text(count);
+        }
+    })
 })
 
-$("#update2").click(function () {
-    if (count <= 0) {
-        $('#counter').text(count);
-    }
-    else {
-        count--;
-        $('#counter').text(count);
-    }
-})
+
+//--- Internal functions
+function ajaxHelper(uri, method, data) {
+    self.error(''); // Clear error message
+    return $.ajax({
+        type: method,
+        dataType: 'json',
+        contentType: 'application/json',
+        url: uri,
+        data: data ? JSON.stringify(data) : null,
+        error: function (jqXHR, textStatus, errorThrown) {
+            console.log("AJAX Call[" + uri + "] Fail...");
+            hideLoading();
+            self.error(errorThrown);
+        }
+    });
+}
+
+function sleep(milliseconds) {
+    const start = Date.now();
+    while (Date.now() - start < milliseconds);
+}
+
+//--- start ....
+showLoading();
+var pg = getUrlParameter('page');
+console.log(pg);
+if (pg == undefined)
+    self.activate(1);
+else {
+    self.activate(pg);
+};
+
+$(document).ready(function () {
+    console.log("ready!");
+    ko.applyBindings(new vm());
+});
