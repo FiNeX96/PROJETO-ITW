98a425fc8a79da9b9e62fa591aecdb57f4f0863e
filipe.obrist@ua.pt <filipe.obrist@ua.pt>
2022-01-20 21:52:54 +0000

    jh

--- Projeto/tickets.js
+++ Projeto/tickets.js
@@ -3,55 +3,50 @@
     var count = 0;
 
     $("#update1").click(function () {
-        count++;
-        $('#counter').text(count);
-    })
-
-    $("#update2").click(function () {
-        if (count <= 0) {
+        if (count > 49) {
             $('#counter').text(count);
         }
         else {
-            count--;
+            count++;
             $('#counter').text(count);
         }
+
+        $("#update2").click(function () {
+            if (count <= 1) {
+                $('#counter').text(count);
+            }
+            else {
+                count--;
+                $('#counter').text(count);
+            }
+        })
     })
-})
 
-
-//--- Internal functions
-function ajaxHelper(uri, method, data) {
-    self.error(''); // Clear error message
-    return $.ajax({
-        type: method,
-        dataType: 'json',
-        contentType: 'application/json',
-        url: uri,
-        data: data ? JSON.stringify(data) : null,
-        error: function (jqXHR, textStatus, errorThrown) {
-            console.log("AJAX Call[" + uri + "] Fail...");
-            hideLoading();
-            self.error(errorThrown);
+    $('#submit').click(function () {
+        var retVal = true;
+        if ($('#inputName').length < 3) {
+            retVal = false;
+            $('#NameError').addClass('d-block');
+            $('#NameError').removeClass('d-none');
+            }
+        else {
+            $('#NameError').removeClass('d-block');
+            $('#NameError').addClass('d-none');
         }
+
+
+
+        if ($('#inputEmail').length < 3) {
+            retVal = false;
+            $('#EmailError').addClass('d-block');
+            $('#EmailError').removeClass('d-none');
+        }
+        else {
+            $('#EmailError').removeClass('d-block');
+            $('#EmailError').addClass('d-none');
+        }
+
+        return retVal;
     });
-}
 
-function sleep(milliseconds) {
-    const start = Date.now();
-    while (Date.now() - start < milliseconds);
-}
-
-//--- start ....
-showLoading();
-var pg = getUrlParameter('page');
-console.log(pg);
-if (pg == undefined)
-    self.activate(1);
-else {
-    self.activate(pg);
-};
-
-$(document).ready(function () {
-    console.log("ready!");
-    ko.applyBindings(new vm());
-});
+})
