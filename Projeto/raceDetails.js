dd33a0b525b1dc0b719c80db4b32b0d7660b8750
filipe.obrist@ua.pt <filipe.obrist@ua.pt>
2022-01-30 16:41:43 +0000

    as

--- Projeto/raceDetails.js
+++ Projeto/raceDetails.js
@@ -3,10 +3,13 @@
     //---Variáveis locais
     var self = this;
     self.baseUri = ko.observable('http://192.168.160.58/Formula1/api/Races/Race?id=');
+    self.baseUri2 = ko.observable('http://192.168.160.58/Formula1/api/Drivers/Driver?id=');
+
     self.displayName = 'Constructor Details';
     self.error = ko.observable('');
     self.passingMessage = ko.observable('');
     //--- Data Record
+    self.ImageUrl = ko.observable('');
     self.RaceId = ko.observable('');
     self.Circuit = ko.observable('');
     self.Year = ko.observable('');
@@ -21,6 +24,7 @@
     self.activate = function (id) {
         console.log('CALL: getRaceDetails...');
         var composedUri = self.baseUri() + id;
+        var composedUri2 = self.baseUri2() + id;
         ajaxHelper(composedUri, 'GET').done(function (data) {
             self.RaceId(data.RaceId);
             self.Circuit(data.Circuit);
@@ -30,11 +34,27 @@
             self.Date(data.Date);
             self.Time(data.Time);
             self.Url(data.Url);
+            self.ImageUrl(data.ImageUrl);
             self.Results(data.Results);
             hideLoading();
-
+        });
+        ajaxHelper(composedUri2, 'GET').done(function (data) {
+            self.Wins(data.Wins)
+            position = data.Career[0].Position
+            function suffix(value) {
+                if (value == 1 || value == 21 || value == 31 || value == 41 || value == 51)
+                    return value + "st";
+                else if (value == 2 || value == 22 || value == 32 || value == 42 || value == 52)
+                    return value + "nd";
+                else if (value == 3 || value == 23 || value == 33 || value == 43 || value == 53)
+                    return value + "rd";
+                else
+                    return value + "th";
+            }
+            self.LastPos(suffix(position));
         });
     };
+ 
     //--- Internal functions
     function ajaxHelper(uri, method, data) {
         self.error(''); // Clear error message
